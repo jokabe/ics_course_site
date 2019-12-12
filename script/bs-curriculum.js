4af07dd66a3e38d645aaf8d1b4e8ffb21fb81051
Jun <okabej3@gmail.com>
2019-12-12 02:49:49 -1000

    File Organization Clean-up
    - Comments are added
    - Transfer Javascript code to bs-curriculum.js

---
+++ script/bs-curriculum.js
@@ -0,0 +1,82 @@
+/* Javascript File used to create the Vis.js graph network in the ICS Curriculum page
+ * Implemented by Jun Okabe
+ */
+
+// graph nodes
+const courses = [
+    {id: 1, level: 1, label: 'ICS111, ICS141'},
+    {id: 2, level: 2, label: 'ICS211, ICS241'},
+    {id: 3, level: 3, label: 'ICS212, ICS311, ICS314'},
+    {id: 4, level: 4, label: 'ICS312, ICS313, ICS321, ICS332'},
+    {id: 5, level: 4, label: 'ICS312, ICS361, ICS321, ICS332'},
+    {id: 6, level: 4, label: 'ICS331, ICS313, ICS321, ICS332'},
+    {id: 7, level: 4, label: 'ICS331, ICS361, ICS321, ICS332'},
+    {id: 8, level: 5, label: 'Five 400-level ICS Courses'},
+]
+
+// graph edges
+const courselinks = [
+    {from: 1, to: 2},
+    {from: 2, to: 3},
+    {from: 3, to: 4},
+    {from: 3, to: 5},
+    {from: 3, to: 6},
+    {from: 3, to: 7},
+    {from: 4, to: 8},
+    {from: 5, to: 8},
+    {from: 6, to: 8},
+    {from: 7, to: 8},
+]
+
+// place the graph at div with "bachelor-science-network" id
+const container = document.getElementById('bachelor-science-network');
+
+// set up graph object
+const data = {
+    nodes: new vis.DataSet(courses),
+    edges: new vis.DataSet(courselinks)
+};
+
+// customize graph object
+const options = {
+    // node customization
+    nodes: {
+        shape: 'dot',
+        size: 25,
+        color : {
+            background: '#36d1dc'
+        },
+        font: {
+          size: 22,
+          color: '#ffffff'
+        },
+        borderWidth: 2
+    },
+
+    // edge customization
+    edges: {
+        arrows: 'to',
+        width: 2,
+        hightlight: '#000428'
+    },
+
+    // setup the orientation of the graph
+    layout: {
+        hierarchical: {
+            direction: 'LR',
+            nodeSpacing: 20,
+            levelSeparation: 300,
+        }
+    },
+
+    // other graph customization (Ex. graph movement, etc.)
+    physics: {
+        hierarchicalRepulsion: {
+            nodeDistance: 200, // distance between each node
+            stabilization: true // stabilize on load
+        }
+    }
+};
+
+// create the graph network
+const network = new vis.Network(container, data, options);
